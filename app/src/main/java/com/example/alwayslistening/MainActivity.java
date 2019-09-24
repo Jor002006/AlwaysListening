8df44b3e4dbf4358981572cb213cbefac416465a
Jorge Lara <lar19449@uvg.edu.gt>
2019-09-23 19:39:49 -0600

    Implementado el metodo para reconocer la voz

--- app/src/main/java/com/example/alwayslistening/MainActivity.java
+++ app/src/main/java/com/example/alwayslistening/MainActivity.java
@@ -2,16 +2,27 @@ package com.example.alwayslistening;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
+import android.net.Uri;
 import android.os.Bundle;
+import android.speech.RecognizerIntent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
+
 
 
 public class MainActivity extends AppCompatActivity {
 
     Button NuevaPalabra;
     Button MisPalabras;
+    Button btnVoice;
+    private static final int VOICE_RECOGNITION_REQUEST_CODE = 1234;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,6 +40,7 @@ public class MainActivity extends AppCompatActivity {
                 AbrirAgregarPantalla();
             }
         });
+        inicializarReconocimiento();
     }
 
     public void AbrirAgregarPantalla ()
@@ -53,5 +65,50 @@ public class MainActivity extends AppCompatActivity {
     public void DameClick(View view) {
 
 
+    }
+
+    //Metodo pa´ reconocer la voz por si las moscas
+    private void inicializarReconocimiento(){
+
+        btnVoice = (Button) findViewById(R.id.btnVoice);
+
+        PackageManager pm = getPackageManager();
+
+
+        List<ResolveInfo> activities = pm.queryIntentActivities(new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH), 0);
+
+        if(activities.size() != 0){
+            btnVoice.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    startVoiceRecognitionActivity();
+                }
+            });
+        }
+    }
+
+    private void startVoiceRecognitionActivity(){
+        Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+        intent.putExtra(RecognizerIntent.EXTRA_PROMPT,"");
+        startActivityForResult(intent, VOICE_RECOGNITION_REQUEST_CODE);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data){
+        if (requestCode == VOICE_RECOGNITION_REQUEST_CODE && resultCode == RESULT_OK){
+            ArrayList<String> matches = data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
+            final CharSequence[] items = new CharSequence[matches.size()];
+            for(int i = 0; i < matches.size(); i++){
+                items[i] = matches.get(i);
+            }
+            Toast yo = Toast.makeText(getApplicationContext(), items[0], Toast.LENGTH_LONG);
+            yo.show();
+            String palabra;
+            palabra = items[0].toString();
+            Toast tosty = Toast.makeText(getApplicationContext(), palabra, Toast.LENGTH_LONG);
+            tosty.show();
+        }
+        super.onActivityResult(requestCode, resultCode, data);
     }
 }
