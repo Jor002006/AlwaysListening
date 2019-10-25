172359b0f31dbb836faa0191f4a09eaa276b6f10
pablofermarroquin <pablofermarroquin@gmail.com>
2019-10-25 12:04:11 -0600

    Primeras funciones de Eliminacion y Silenciado de palabras

--- app/src/main/java/com/example/alwayslistening/MisPalabras.java
+++ app/src/main/java/com/example/alwayslistening/MisPalabras.java
@@ -25,21 +25,71 @@ import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListAdapter;
 import android.widget.ListView;
 import android.widget.Toast;
+import java.util.ArrayList;
 
 public class MisPalabras extends AppCompatActivity {
 
 
     ListView LV;
+    ArrayList<String> listaInformacion;
+    ArrayList<Palabra> listaPalabras;
+    ConexionSQLiteHelper conn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mis_palabras);
 
+        conn = new ConexionSQLiteHelper(getApplicationContext(),com.example.alwayslistening.utilidades.NOMBRE_BD,null, 1);
+
+        LV = (ListView) findViewById(R.id.lv1);
+         consultarListaPalabras();
+
+         ArrayAdapter adaptador = new ArrayAdapter(this, android.R.layout.simple_list_item_1, listaInformacion);
+         LV.setAdapter(adaptador);
+
+         LV.setOnItemClickListener(new AdapterView.OnItemClickListener()
+             {
+                 @Override
+                 public void onItemClick(AdapterView<?> adapterView, View view, int pos, long l)
+                 {
+                     String textoPalabra = listaPalabras.get(pos).getTextoPalabra()+"\n";
+                     String estado=(listaPalabras.get(pos).getActivada()==1)?"Activada":"Silencjo";//listaPalabras.get(pos).getPatronVibracion();
+                    MostrarPestana(textoPalabra,estado);
+
+                 }
+             }
+         );
+    }
+
+    private void consultarListaPalabras()
+    {
+        SQLiteDatabase db = conn.getReadableDatabase();
+        Palabra palabra = null;
+        listaPalabras = new ArrayList<Palabra>();
+        Cursor cursor = db.rawQuery("SELECT * FROM "+com.example.alwayslistening.utilidades.TABLA_PALABRA, null);
+
+        while(cursor.moveToNext())
+        {
+            palabra = new Palabra(cursor.getInt(0), cursor.getString(1),cursor.getInt(2), cursor.getString(3));
+            listaPalabras.add(palabra);
+        }
+
+        obtenerListaInformacion();
+    }
+
+    private void obtenerListaInformacion()
+    {
+        listaInformacion = new ArrayList<String>();
+        for(int i=0;i<listaPalabras.size();i++)
+        {
+            listaInformacion.add(listaPalabras.get(i).getTextoPalabra()+" ");
+        }
     }
 
     //toma de la base de datos todas las palabras registradas.
@@ -75,8 +125,23 @@ public class MisPalabras extends AppCompatActivity {
         AlertDialog.Builder dialogo1 = new AlertDialog.Builder(this);
         dialogo1.setTitle("Mis Palabras");
         dialogo1.setMessage(PalabrasQueHay());
-
         dialogo1.show();
+    }
+
+    private void MostrarPestana(final String title, String msg)
+    {
+        AlertDialog.Builder dialogo1 = new AlertDialog.Builder(this);
+        dialogo1.setTitle(title);
+        dialogo1.setMessage("ESTADO: "+msg);
+
+        final String primerBoton=msg.equalsIgnoreCase("Activada")?"SILENCIAR":"ACTIVAR";
+        dialogo1.setPositiveButton(primerBoton, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        String desplegado= primerBoton.equalsIgnoreCase("SILENCIAR")?"["+title+"] silenciada":"["+title+"] activada";
+                    }
+                });
+                dialogo1.show();
     }
 
     /*public ArrayList getAllWords() {
