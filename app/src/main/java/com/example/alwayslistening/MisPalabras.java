db4fccb3c5092bd7fc4bba9bf41dc770cd7a3168
pablofermarroquin <pablofermarroquin@gmail.com>
2019-10-25 19:25:40 -0600

    Silenciar y eliminar palabras COMPLETO

--- app/src/main/java/com/example/alwayslistening/MisPalabras.java
+++ app/src/main/java/com/example/alwayslistening/MisPalabras.java
@@ -52,22 +52,33 @@ public class MisPalabras extends AppCompatActivity {
         LV = (ListView) findViewById(R.id.lv1);
          consultarListaPalabras();
 
-         ArrayAdapter adaptador = new ArrayAdapter(this, android.R.layout.simple_list_item_1, listaInformacion);
-         LV.setAdapter(adaptador);
+         PonerAdapter();
 
          LV.setOnItemClickListener(new AdapterView.OnItemClickListener()
              {
                  @Override
                  public void onItemClick(AdapterView<?> adapterView, View view, int pos, long l)
                  {
-                     palabraActual = listaPalabras.get(pos);
-                     String textoPalabra = listaPalabras.get(pos).getTextoPalabra();
-                     String estado=(listaPalabras.get(pos).getActivada()==1)?"Activada":"Silencio";//listaPalabras.get(pos).getPatronVibracion();
-                    MostrarPestana(textoPalabra,estado);
+                     try {
+                         palabraActual = listaPalabras.get(pos);
+                         String textoPalabra = listaPalabras.get(pos).getTextoPalabra();
+                         String estado = (listaPalabras.get(pos).getActivada() == 1) ? "Activada" : "Silencio";//listaPalabras.get(pos).getPatronVibracion();
+                         MostrarPestana(textoPalabra, estado);
+                     }
+                     catch(Exception e)
+                     {
+                         Toast.makeText(getApplicationContext(),e.getMessage(),Toast.LENGTH_LONG).show();
+                     }
 
                  }
              }
          );
+    }
+
+    private void PonerAdapter()
+    {
+        ArrayAdapter adaptador = new ArrayAdapter(this, android.R.layout.simple_list_item_1, listaInformacion);
+        LV.setAdapter(adaptador);
     }
 
     private void consultarListaPalabras()
@@ -91,7 +102,8 @@ public class MisPalabras extends AppCompatActivity {
         listaInformacion = new ArrayList<String>();
         for(int i=0;i<listaPalabras.size();i++)
         {
-            listaInformacion.add(listaPalabras.get(i).getTextoPalabra()+" ");
+            String estado=(listaPalabras.get(i).getActivada()==0)?"SILENCIO":"ACTIVADA";
+            listaInformacion.add(listaPalabras.get(i).getTextoPalabra()+"    ["+estado+"]");
         }
     }
 
@@ -133,6 +145,7 @@ public class MisPalabras extends AppCompatActivity {
 
     private void MostrarPestana( String title, String msg)
     {
+        try{
         final boolean PalabraActivada=msg.equalsIgnoreCase("Activada");
         AlertDialog.Builder dialogo1 = new AlertDialog.Builder(this);
         dialogo1.setTitle(title);
@@ -142,20 +155,56 @@ public class MisPalabras extends AppCompatActivity {
         dialogo1.setPositiveButton(primerBoton, new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialogInterface, int i) {
-                        String desplegado= (PalabraActivada)?"["+palabraActual.getTextoPalabra()+"] silenciada":"["+palabraActual.getTextoPalabra()+"] activada";
-                        int nuevoEstado=(PalabraActivada)?0:1;
-                        SQLiteDatabase db=conn.getReadableDatabase();
-                        String[] parametros={palabraActual.getTextoPalabra()};//title contiene la palabra
-                        ContentValues cv = new ContentValues();
-                        cv.put(com.example.alwayslistening.utilidades.CAMPO_ID,palabraActual.getIdPalabra());
-                        cv.put(com.example.alwayslistening.utilidades.CAMPO_TEXTO,palabraActual.getTextoPalabra());
-                        cv.put(com.example.alwayslistening.utilidades.CAMPO_ACTIVADA,nuevoEstado);
-                        cv.put(com.example.alwayslistening.utilidades.CAMPO_PATRON_VIBRACION,palabraActual.getPatronVibracion());
 
-                        db.update(com.example.alwayslistening.utilidades.TABLA_PALABRA, cv, com.example.alwayslistening.utilidades.CAMPO_TEXTO+"='"+palabraActual.getTextoPalabra()+"'", null);
+                        try {
+                            String desplegado = (PalabraActivada) ? "[" + palabraActual.getTextoPalabra() + "] silenciada" : "[" + palabraActual.getTextoPalabra() + "] activada";
+                            int nuevoEstado = (PalabraActivada) ? 0 : 1;
+                            SQLiteDatabase db = conn.getReadableDatabase();
+                            String[] parametros = {palabraActual.getTextoPalabra()};//title contiene la palabra
+                            ContentValues cv = new ContentValues();
+                            cv.put(com.example.alwayslistening.utilidades.CAMPO_ID, palabraActual.getIdPalabra());
+                            cv.put(com.example.alwayslistening.utilidades.CAMPO_TEXTO, palabraActual.getTextoPalabra());
+                            cv.put(com.example.alwayslistening.utilidades.CAMPO_ACTIVADA, nuevoEstado);
+                            cv.put(com.example.alwayslistening.utilidades.CAMPO_PATRON_VIBRACION, palabraActual.getPatronVibracion());
+
+                            db.update(com.example.alwayslistening.utilidades.TABLA_PALABRA, cv, com.example.alwayslistening.utilidades.CAMPO_TEXTO + "='" + palabraActual.getTextoPalabra() + "'", null);
+                            consultarListaPalabras();
+                            PonerAdapter();
+                            String mensajeToast = (PalabraActivada) ? "Palabra silenciada" : "Palabra activada";
+                            Toast.makeText(getApplicationContext(), mensajeToast, Toast.LENGTH_LONG).show();
+                        }
+                        catch(Exception e)
+                        {
+                            Toast.makeText(getApplicationContext(),e.getMessage(),Toast.LENGTH_LONG).show();
+                        }
                     }
                 });
+
+        dialogo1.setNegativeButton("ELIMINAR", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+
+                try{
+                SQLiteDatabase db=conn.getReadableDatabase();
+                String[] parametros={palabraActual.getTextoPalabra()};//title contiene la palabra
+                db.delete(com.example.alwayslistening.utilidades.TABLA_PALABRA, com.example.alwayslistening.utilidades.CAMPO_TEXTO+"='"+palabraActual.getTextoPalabra()+"'", null);
+                consultarListaPalabras();
+                PonerAdapter();
+                String mensajeToast="Palabra eliminada";
+                Toast.makeText(getApplicationContext(),mensajeToast,Toast.LENGTH_LONG).show();
+                }
+                catch(Exception e)
+                {
+                    Toast.makeText(getApplicationContext(),e.getMessage(),Toast.LENGTH_LONG).show();
+                }
+            }
+        });
                 dialogo1.show();
+        }
+        catch(Exception e)
+        {
+            Toast.makeText(getApplicationContext(),e.getMessage(),Toast.LENGTH_LONG).show();
+        }
     }
 
     /*public ArrayList getAllWords() {
